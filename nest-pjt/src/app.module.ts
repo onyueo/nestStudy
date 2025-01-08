a902fd8647148fde6264248eb2bdfcba0e978f97
On Yu <gkc04075@gmail.com>
2025-01-09 00:39:07 +0900

    feat: post 컨트롤러 서비스 생성

--- nest-pjt/src/app.module.ts
+++ nest-pjt/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
